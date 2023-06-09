9a6ca6f486e2fd6f38bf1a640079b566863be9e2
Frost101 <1905101@ugrad.cse.buet.ac.bd>
2023-06-09 20:41:01 +0600

    motamuti done

--- Offline-1(Topic-A star Search)/code/Astar.cpp
+++ Offline-1(Topic-A star Search)/code/Astar.cpp
@@ -1,6 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int direction[4][2] = {{-1,0},{1,0},{0,-1},{0,1}};  //Up-Down-Left-Right
+
+
 
 class Npuzzle{
 
@@ -8,12 +11,20 @@ public:
     vector<vector<int>> board;
     int hamDistance;
     int manDistance;
+    int priority;
+    int depth;
     Npuzzle *parent;
 
-    Npuzzle(vector<vector<int>> &board){
+    Npuzzle(vector<vector<int>> board,int depth){
         this->board = board;
+        this->depth = depth;
         hamDistance = this->hamming_distance();
         manDistance = this->manhattan_distance();
+        this->priority = manDistance + depth;
+    }
+
+    void calculate_priority(){
+        priority = manDistance + depth;
     }
 
     bool is_solvable(){
@@ -40,6 +51,7 @@ public:
                 }
             }
         }
+        hamDistance = distance;
         return distance;
     }
 
@@ -56,6 +68,7 @@ public:
                 }
             }
         }
+        manDistance = distance;
         return distance;
     }
 
@@ -94,6 +107,27 @@ public:
         }
         return temp;
     }
+
+    void print(){
+        int N = board.size();
+        cout << endl;
+        for(int i=0; i<N; i++){
+            for(int j=0; j<N; j++){
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+        cout << endl;
+    }
+
+};
+
+
+class Comparator{
+    public:
+        bool operator()(Npuzzle* x,Npuzzle* y){
+            return x->priority > y->priority;
+        }
 };
 
 int main()
@@ -109,6 +143,59 @@ int main()
         }
     }
 
-    Npuzzle npuzzle(board);
-    cout << npuzzle.is_solvable()<< endl;
+    int moves = 0;
+    priority_queue<Npuzzle*,vector<Npuzzle*>,Comparator> pq;
+    Npuzzle* npuzzle = new Npuzzle(board,moves);
+    pq.push(npuzzle);
+
+
+    if(!npuzzle->is_solvable()){
+        cout << "Unsolvable Puzzle" << endl;
+        return 0;
+    }
+
+    Npuzzle* ans;
+
+    while(!pq.empty()){
+        moves++;
+        Npuzzle* tmp = pq.top();
+        pq.pop();
+
+        if(tmp->hamming_distance()==0){
+            cout << tmp->depth << endl;
+            ans = tmp;
+            break;
+        }
+
+        pair<int,int> zero_block = tmp->find_zero_block();
+        int old_row = zero_block.first;
+        int old_column = zero_block.second;
+
+        for(int i=0; i<4; i++){
+            int new_row = old_row + direction[i][0];
+            int new_column = old_column + direction[i][1];
+            if(new_row >= 0 && new_row < N && new_column >= 0 && new_column < N){
+                Npuzzle* child = new Npuzzle(tmp->board,moves);
+                swap(child->board[old_row][old_column],child->board[new_row][new_column]);
+                child->hamming_distance();
+                child->manhattan_distance();
+                child->calculate_priority();
+                child->parent = tmp;
+                pq.push(child);
+            }
+        }
+    }
+
+    stack<Npuzzle*> stk;
+    stk.push(ans);
+    while(ans->parent!=NULL){
+        stk.push(ans->parent);
+        ans = ans->parent;
+    }
+
+    while(!stk.empty()){
+        stk.top()->print();
+        stk.pop();
+    }
+
 }
